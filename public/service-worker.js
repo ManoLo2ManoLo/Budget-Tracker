a1d4e601521fbf539bc5da72bae2ddaafa9bd36e
ManoLo2ManoLo <manuel_canas@aol.com>
2022-01-17 10:51:14 -0500

    forget to add something to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
     './manifest.json',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    ''
+    '/'
 ]
 
 self.addEventListener('fetch', function (e) {
